f0fc1c1b57b45cffd8269764524664294f6deec7
abj95630 <abah20000@gmail.com>
2023-03-02 23:34:38 +0100

    differents changements et ajouts

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 window.onscroll = function() {scrollFunction()};
+let mybutton = document.getElementById("myBtn");
 
 function scrollFunction() {
-  if (document.documentElement.scrollTop < 890) {
-    document.getElementById("navbar").style.position = "relative";
+  if (document.documentElement.scrollTop > 30 || document.body.scrollTop > 30) {
+    mybutton.style.display = "block";
   } else {
-    document.getElementById('navbar').style.position = "fixed";
+    mybutton.style.display = "none";
   }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
 }

0a7d63e1967e341acf86846a0270b46672f86283
abj95630 <abah20000@gmail.com>
2023-03-03 17:49:59 +0100

    Agencements de différents éléments

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onscroll = function() {scrollFunction()};
 let mybutton = document.getElementById("myBtn");
 
 function scrollFunction() {
-  if (document.documentElement.scrollTop > 30 || document.body.scrollTop > 30) {
+  if (document.documentElement.scrollTop > 450 || document.body.scrollTop > 450) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
